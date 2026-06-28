fde712a9f3b9944ef49226392b0c344d3e83e723
Mehita Jose <mehitajose@gmail.com>
2026-06-28 18:15:02 +0530

    Added my first python project

---
+++ 10 quizzes.py
@@ -0,0 +1,54 @@
+input ("welcome to computer quiz !")
+playing = input("do you want to play?")
+if (playing =="yes"):
+    print(" okay!!! lets  play :)")
+answer= input("What does CPU stands for ?")
+if(answer== "central processing unit"):
+     print("SUCCESS")
+else :
+     print("TRY AGAIN")
+answer = input("what does GIU stands for ")
+if (answer=="graphical interface unit"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input ("What is the capital city of France?")
+if (answer=="paris"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input("What is the largest ocean in the world?")
+if (answer=="pacific ocean"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input ("Which gas do humans primarily breathe in?")
+if (answer=="pacific ocean"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input(" What is the only even prime number?")
+if (answer=="2"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input("Who wrote the Harry Potter series?")
+if (answer=="JK.Rowling "):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input("What is acrophobia a fear of? ")
+if (answer=="heights"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input("How many elements are in the periodic table?")
+if (answer=="118"):
+    print("SUCCESS")
+else:
+    print("FAIL")
+answer = input(" How many bones do we have in an ear?")
+if (answer=="3"):
+    print("SUCCESS")
+else:
+    print("FAIL")
